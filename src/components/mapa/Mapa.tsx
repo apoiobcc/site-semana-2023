7cb2db143ce03bf0521f2bcf371c44645257c52f
Jonathas <jonathascastilhoes@gmail.com>
2023-07-31 21:28:19 -0300

    Criação do mapa

---
+++ src/components/mapa/Mapa.tsx
@@ -0,0 +1,10 @@
+export default function () {
+    return (
+        <div className="Mapa__container">
+            <span className="Mapa__title">Como Chegar?</span>
+            <div className="Mapa__maps">
+                Enfia o mapa do google aqui
+            </div>
+        </div>
+    )
+}
