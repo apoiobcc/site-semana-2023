124ee4baf4b3c13934994e5fb7be2ea38ab51521
yesmanic <yesman.choque@gmail.com>
2023-09-04 18:44:57 -0300

    Update carrossel

--- src/components/apresentacao/Carrossel.tsx
+++ src/components/apresentacao/Carrossel.tsx
@@ -4,7 +4,10 @@ import Image from "next/image"
 import { useState } from "react"
 
 interface props {
-    images: {url: string}[]
+    images: {
+        url: string,
+        description: string
+    }[]
 }
 
 const Carrossel = ({ images } : props) => {
@@ -22,25 +25,28 @@ const Carrossel = ({ images } : props) => {
     }
     
     return (
-        <div className="overflow-hidden relative">
-            <div className="flex h-[700px] w-full duration-500">
-                <Image 
-                    src={images[imageIndex].url} 
-                    height={700} 
-                    width={1500} 
-                    alt="Image" 
-                    className="object-cover w-full transition-all"
-                />
-            </div>
-            <div className="absolute inset-0 flex items-center justify-between p-4">
-                <button onClick={prevImage}>
-                    <img src="/images/left-arrow.svg" alt="" height={40} className="bg-yellow-300 p-3 border-[0.3rem] border-black"/>
-                </button>
+        <div className="flex flex-col">
+            <div className="flex overflow-hidden relative border-black border-[0.4rem]">
+                <div className="carousel-image">
+                    <Image 
+                        src={images[imageIndex].url} 
+                        height={700} 
+                        width={1500} 
+                        alt="Image" 
+                        className="object-cover object-center"
+                    />
+                </div>
+                <div className="absolute inset-0 flex items-center justify-between p-4">
+                    <button onClick={prevImage}>
+                        <img src="/images/left-arrow.svg" alt="" height={40} className="bg-yellow-300 p-3 border-[0.3rem] border-black w-[8vw] max-w-[70px]"/>
+                    </button>
 
-                <button onClick={nextImage}>
-                    <img src="/images/right-arrow.svg" alt="" height={40} className="bg-yellow-300 p-3 border-[0.3rem] border-black"/>
-                </button>
+                    <button onClick={nextImage}>
+                        <img src="/images/right-arrow.svg" alt="" height={40} className="bg-yellow-300 p-3 border-[0.3rem] border-black w-[8vw] max-w-[70px]"/>
+                    </button>
+                </div>
             </div>
+            <p className="carousel-description">{images[imageIndex].description}</p>
         </div>
     )
 }
--- src/components/apresentacao/Sobre.tsx
+++ src/components/apresentacao/Sobre.tsx
@@ -4,13 +4,16 @@ import Carrossel from "./Carrossel";
 const Sobre = () => {
     const images = [
         {
-            url : "https://images.unsplash.com/photo-1691539706978-3cb89d88915f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5MzY1Nzc5NA&ixlib=rb-4.0.3&q=80&w=1080"
+            url: "https://images.unsplash.com/photo-1691539706978-3cb89d88915f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5MzY1Nzc5NA&ixlib=rb-4.0.3&q=80&w=1080",
+            description: "A semana da computação é uma semana de palestras que aborda diversos temas relacionados às áreas da computação. O evento é aberto a todo público e qualquer interessado pode assistir às palestras."
         },
         {
-            url : "https://plus.unsplash.com/premium_photo-1691852519415-0df4c22e49e1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5MzY1NzgzNQ&ixlib=rb-4.0.3&q=80&w=1080"
+            url: "https://plus.unsplash.com/premium_photo-1691852519415-0df4c22e49e1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5MzY1NzgzNQ&ixlib=rb-4.0.3&q=80&w=1080",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sem nulla, auctor at metus sed, malesuada vulputate nisl. Nulla ultrices."
         },
         {
-            url : "https://images.unsplash.com/photo-1692481130055-5508b8ba260d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5MzY1Nzg1MQ&ixlib=rb-4.0.3&q=80&w=1080"
+            url: "https://images.unsplash.com/photo-1692481130055-5508b8ba260d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5MzY1Nzg1MQ&ixlib=rb-4.0.3&q=80&w=1080",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed mauris elit. Interdum et malesuada fames ac ante ipsum primis."
         },
     ]
 
@@ -19,11 +22,9 @@ const Sobre = () => {
             <div className="titulo-rosa">
                 SOBRE A SEMANA
             </div>
-            <div className="border-black border-[0.4rem]">
+            <div>
                 <Carrossel images={images}/>
             </div>
-
-            <div className="my-8">A semana da computação é uma semana de palestras que aborda diversos temas relacionados às áreas da computação. O evento é aberto a todo público e qualquer interessado pode assistir às palestras.</div>
         </div>
     )
 }
